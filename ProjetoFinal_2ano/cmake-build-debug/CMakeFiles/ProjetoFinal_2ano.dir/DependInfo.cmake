43f7178a67f67e8c84db123e4ba11a03397cd208
diferreira2000 <57480525+diferreira2000@users.noreply.github.com>
2020-12-26 19:54:09 +0000

    Projeto final 2 ano

---
+++ ProjetoFinal_2ano/cmake-build-debug/CMakeFiles/ProjetoFinal_2ano.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/asus/CLionProjects/ProjetoFinal_2ano/Functions.c" "C:/Users/asus/CLionProjects/ProjetoFinal_2ano/cmake-build-debug/CMakeFiles/ProjetoFinal_2ano.dir/Functions.c.obj"
+  "C:/Users/asus/CLionProjects/ProjetoFinal_2ano/Ide.c" "C:/Users/asus/CLionProjects/ProjetoFinal_2ano/cmake-build-debug/CMakeFiles/ProjetoFinal_2ano.dir/Ide.c.obj"
+  "C:/Users/asus/CLionProjects/ProjetoFinal_2ano/main.c" "C:/Users/asus/CLionProjects/ProjetoFinal_2ano/cmake-build-debug/CMakeFiles/ProjetoFinal_2ano.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ProjetoFinal_2ano/cmake-build-debug/CMakeFiles/ProjetoFinal_2ano.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoFinal_2ano.dir/Functions.c.obj"
+  "CMakeFiles/ProjetoFinal_2ano.dir/Ide.c.obj"
+  "CMakeFiles/ProjetoFinal_2ano.dir/main.c.obj"
+  "ProjetoFinal_2ano.exe"
+  "ProjetoFinal_2ano.exe.manifest"
+  "ProjetoFinal_2ano.pdb"
+  "libProjetoFinal_2ano.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetoFinal_2ano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
